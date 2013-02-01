4f30006a7b6b949009eed5f2514c654d8e3317fe
Diederik van Liere <dvanliere@wikimedia.org>
2013-02-01 16:57:26 -0500

    Script to add continent names and acronyms
    Change-Id: I4cdde15ceed45dd4c591b3edf311e21ee121764c

---
+++ geo/continent.py
@@ -0,0 +1,51 @@
+
+import json
+
+def create_country_dict(json_data):
+	countries = {}
+	for country in json_data:
+		a2 = country.get('a2')
+		countries[a2] = country
+		print country
+	return countries
+
+def create_continent_dict(fh):
+	continents = {}
+	for line in fh:
+		continent, name = line.strip().split(',')
+		continents[continent] = name.strip()
+	fh.close()
+	return continents
+
+def main():
+	fh1 = open('country-codes.json', 'r')
+	fh2 = open('a2_continent.csv', 'r')
+	fh3 = open('continent_name.csv', 'r')
+
+	json_data = json.load(fh1)
+	fh1.close()
+
+	countries = create_country_dict(json_data)
+	continents = create_continent_dict(fh3)
+
+	for line in fh2:
+		a2, continent = line.strip().split(',')
+		
+		country  = countries.get(a2)
+		if country:
+			country['continent'] = continent
+			country['continent_name'] = continents.get(continent)
+
+	fh2.close()
+
+	#print json.dumps(countries.values(), indent=4, separators=(',', ': '))
+	#print json.dumps(countries.values(), indent=-1,separators=(',', ': '))
+
+	print '['
+	for country in countries.values():
+		print '\t %s,' % (json.dumps(country))
+	print ']'
+
+
+if __name__ == '__main__':
+	main()
