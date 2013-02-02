e4e2686120e9be6e58215ac71c91777d82675a2a
Diederik van Liere <dvanliere@wikimedia.org>
2013-02-02 12:40:33 -0500

    Add csv writer
    Change-Id: I87575f8f7a7a209400ffe8b1905053803e334c3c

--- geo/continent.py
+++ geo/continent.py
@@ -1,5 +1,22 @@
 
+import csv
 import json
+
+def write_json(countries):
+	output = open('country-codes.json', 'w')	
+	output.write('[\n')
+        for country in countries.values():
+                output.write('\t %s,\n' % (json.dumps(country, sort_keys=True)))
+        output.write(']\n')	
+	output.close()
+
+def write_csv(countries):
+	output = open('country-codes.csv' 'wb')
+	fieldnames = countries.get('NL').keys()
+	writer = csv.Dictwriter(output,fieldnames, quoting=csv.QUOTE_MINIMAL)
+	for a2, country in countries:iteritems():
+		writer.writerow(country)
+	output.close()
 
 def create_country_dict(json_data):
 	countries = {}
@@ -41,13 +58,10 @@ def main():
 	fh2.close()
 
 	#print json.dumps(countries.values(), indent=4, separators=(',', ': '))
-	#print json.dumps(countries.values(), indent=-1,separators=(',', ': '))
+	print json.dumps(countries.values(), indent=-1,separators=(',', ': '))
 
-	print '['
-	for country in countries.values():
-		print '\t %s,' % (json.dumps(country, sort_keys=True))
-	print ']'
-
+	write_json(countries)
+	write_csv(countries)
 
 if __name__ == '__main__':
 	main()
